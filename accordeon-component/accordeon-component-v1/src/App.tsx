597bbd6f9970021d29ffc8f2d3260c358b47204d
Dmitriy <mr.dima337@gmail.com>
2025-09-13 22:20:48 +0300

    feat: implement accordion v2

--- accordeon-component/accordeon-component-v1/src/App.tsx
+++ accordeon-component/accordeon-component-v1/src/App.tsx
@@ -25,32 +25,37 @@ function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, index) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           num={index}
           title={el.title}
-          text={el.text}
           key={crypto.randomUUID()}
-        />
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = curOpen === num;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
+
   return (
     <div className={`item ${isOpen ? "open" : ""} `} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
