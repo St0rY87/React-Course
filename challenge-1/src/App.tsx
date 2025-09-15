46514efda755083295624e04a2e3e2e107f4f3e5
Dmitriy <mr.dima337@gmail.com>
2025-09-15 23:32:30 +0300

    finished challenge

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -16,6 +16,13 @@ function App() {
     setHowFriendLIke("dissatisfied");
   }
 
+  function calcTips(levelSatisfying: string) {
+    if (levelSatisfying === "dissatisfied") return 0;
+    if (levelSatisfying === "ok") return 5;
+    if (levelSatisfying === "amazing") return 10;
+    if (levelSatisfying === "absolutely") return 20;
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -53,7 +60,16 @@ function App() {
           </select>
         </div>
 
-        <p className="total">You pay X (X + X)</p>
+        <p className="total">
+          {`You pay $${
+            Number(bill) +
+            ((bill / 100) * (calcTips(howYouLIke) + calcTips(howFriendLIke))) /
+              2
+          } ($${bill}  + $${
+            ((bill / 100) * (calcTips(howYouLIke) + calcTips(howFriendLIke))) /
+            2
+          } tip)`}
+        </p>
 
         <button onClick={resetForm}>Reset</button>
       </form>
