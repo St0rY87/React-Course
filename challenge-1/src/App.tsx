0377c8c96e64dc76576e7dbd70e2ecf97400dc8f
Dmitriy <mr.dima337@gmail.com>
2025-09-17 21:10:18 +0300

    refactor: refactor code

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -2,88 +2,98 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  function TipCalculator() {
-    const [bill, setBill] = useState("");
-    const [persentage1, setPersentage1] = useState(0);
-    const [persentage2, setPersentage2] = useState(0);
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
 
-    const tip = bill * ((persentage1 + persentage2) / 2 / 100);
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
 
-    function resetForm() {
-      setBill("");
-      setPersentage1(0);
-      setPersentage2(0);
-    }
+  const tip = ((bill / 100) * (percentage1 + percentage2)) / 2;
 
-    return (
-      <div>
-        <BillInput bill={bill} onSetBill={setBill} />
-        <SelectPercentage persentage={persentage1} onSelect={setPersentage1}>
-          How did you like the service
-        </SelectPercentage>
-        <SelectPercentage persentage={persentage2} onSelect={setPersentage2}>
-          How did your friend like the service ?
-        </SelectPercentage>
-        {bill > 0 && (
-          <>
-            <Output bill={bill} tip={tip} />
-            <Reset onResetForm={resetForm} />
-          </>
-        )}
-      </div>
-    );
-  }
-
-  function BillInput({ bill, onSetBill }) {
-    return (
-      <div>
-        <label>How much was the bill ?</label>
-        <input
-          type="text"
-          placeholder="Bill value"
-          value={bill}
-          onChange={(e) => {
-            onSetBill(Number(e.target.value));
-          }}
-        />
-      </div>
-    );
-  }
-
-  function SelectPercentage({ children, persentage, onSelect }) {
-    return (
-      <div>
-        <label>{children}</label>
-        <select
-          value={persentage}
-          onChange={(e) => onSelect(Number(e.target.value))}
-        >
-          <option value="0">Dissatisfied (0%)</option>
-          <option value="5">It was ok (5%)</option>
-          <option value="10">It was good (10%)</option>
-          <option value="20">Absolutely amazing! (20%)</option>
-        </select>
-      </div>
-    );
-  }
-
-  function Output({ bill, tip }) {
-    return (
-      <h3>
-        You pay ${bill + tip} (${bill} + ${tip} tip)
-      </h3>
-    );
-  }
-
-  function Reset({ onResetForm }) {
-    return <button onClick={onResetForm}>Reset</button>;
+  function resetForm() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
   }
 
   return (
     <>
-      <TipCalculator />
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage
+        percentage={percentage1}
+        onSetPercentage={setPercentage1}
+      >
+        How did you like the service ?
+      </SelectPercentage>
+      <SelectPercentage
+        percentage={percentage2}
+        onSetPercentage={setPercentage2}
+      >
+        How did your friend like the service ?
+      </SelectPercentage>
+      {bill > 0 && (
+        <div>
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={resetForm} />
+        </div>
+      )}
     </>
   );
 }
 
+function BillInput({ bill, onSetBill }) {
+  return (
+    <div>
+      <label>How much was the bill ?</label>
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))} // Keep it as a string
+      />
+    </div>
+  );
+}
+
+function SelectPercentage({ children, percentage, onSetPercentage }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select
+        value={percentage}
+        onChange={(e) => onSetPercentage(Number(e.target.value))}
+      >
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was ok (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ bill, tip }) {
+  return (
+    <>
+      <h3>
+        You pay ${bill + tip} (${bill} + ${tip} tip)
+      </h3>
+    </>
+  );
+}
+
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
+}
+
 export default App;
+
+// How much was the bill ?
+// How did you like the service ?
+// How did your friend like the service ?
