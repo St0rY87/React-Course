bb19369be43c561a8dc6e0c07dc1b3e146391878
Dmitriy <mr.dima337@gmail.com>
2025-09-26 22:52:33 +0300

    feat: displaying form addFriend

--- Eat-'N-Split/src/App.jsx
+++ Eat-'N-Split/src/App.jsx
@@ -22,13 +22,29 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
@@ -68,9 +84,6 @@ function Friend({ friend }) {
       <Button>Select</Button>
     </li>
   );
-}
-function Button({ children }) {
-  return <button className="button">{children}</button>;
 }
 
 function FormAddFriend() {
