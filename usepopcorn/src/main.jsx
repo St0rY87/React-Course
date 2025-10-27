d9ee49864ec318321bb91160e7a32253e3d05b67
Dmitriy <mr.dima337@gmail.com>
2025-10-27 21:07:09 +0300

    feat: make reusable component

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -1,12 +1,22 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import './index.css'
 import StarRating from "./StarRating";
 // import App from "./App.jsx";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return <div>
+    <StarRating color='blue' maxRating={10} onSetMovieRating={setMovieRating}/>
+    <p style={{fontSize: 18}}>This movie was rated {movieRating} stars</p>
+  </div>
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10}/>
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating size={40} color='tomato' defaultRating={4}/>
+    <Test/>
   </StrictMode>
 );
