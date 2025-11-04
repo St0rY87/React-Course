1342cfd80b1641f1fd1e31e3ac41db473dd7b558
Dmitriy <mr.dima337@gmail.com>
2025-11-04 23:13:02 +0300

    feat: add loader

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -203,19 +203,26 @@ function MovieList({ movies }) {
     </ul>
   );
 }
+
+function Loader() {
+  return <p className="loader">Loading...</p>
+}
+
 const KEY = "1cd4c500";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'interstellar';
 
   useEffect(() => {
      async function fetchMovies() {
+      setIsLoading(true)
       const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
       const data = await res.json();
       setMovies(data.Search);
-      console.log(movies)
+      setIsLoading(false);
     }
     fetchMovies()
   }, []);
@@ -229,7 +236,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          { isLoading ? <Loader/> : < MovieList movies={movies} /> }
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
