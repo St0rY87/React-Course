489276776815e163bba91a6f7a35c9b6b8721c1a
Dmitriy <mr.dima337@gmail.com>
2025-11-13 23:26:36 +0300

    init: init app

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  )
+}
+
+export default App
