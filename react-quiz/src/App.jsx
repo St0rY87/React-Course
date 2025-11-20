8103d2521ea1d8afc1e221fd5fe15a5d1f1204e3
Dmitriy <mr.dima337@gmail.com>
2025-11-20 23:25:16 +0300

    feat: implement fetching data from fake api

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import MainContent from "./MainContent";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'ready', 'error', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
