be8ff72873daaf68b76a96f4805d70e4c555e9de
Dmitriy <mr.dima337@gmail.com>
2025-12-07 15:46:56 +0300

    feat: display data of city

--- spa-worldWise/contexts/CitiesContext.jsx
+++ spa-worldWise/contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
@@ -24,18 +25,38 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
       {children}
     </CitiesContext.Provider>
   );
 }
 
 function useCities() {
-    const context = useContext(CitiesContext);
-        if(context === undefined)
-            throw new Error("CitiesContext was used outside the CitiesProvider")
-    return context;
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
 }
 
 export { CitiesProvider, useCities };
