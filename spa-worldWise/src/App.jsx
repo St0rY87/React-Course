6cee5fcdfe476d32c993e674fbaf52c40158dd24
Dmitriy <mr.dima337@gmail.com>
2026-02-15 20:48:59 +0300

    feat spa-worldWise: add styles to pages

--- spa-worldWise/src/App.jsx
+++ spa-worldWise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import './index.css';
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path='login' element={<Login/>}/>
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
