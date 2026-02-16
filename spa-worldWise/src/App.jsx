3fe163208a2d5d3afe3ec5103021499fadd3f56e
Dmitriy <mr.dima337@gmail.com>
2026-02-16 19:14:01 +0300

    feat spa-worldWise: implement displaying countries

--- spa-worldWise/src/App.jsx
+++ spa-worldWise/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
-
-
+import CountryList from "./components/CountryList";
 import PageNotFound from "./pages/PageNotFound";
+
 import "./index.css";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -47,7 +47,7 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />}/>
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- spa-worldWise/src/components/CountryList.jsx
+++ spa-worldWise/src/components/CountryList.jsx
@@ -1,23 +1,52 @@
+// import Spinner from "./Spinner";
+// import Message from "./Message";
+// import styles from "./CountryList.module.css";
+// import CountryItem from "./CountryItem";
+// import { useCities } from "../contexts/CitiesContext";
+
+// function CountryList() {
+//   const { cities, isLoading } = useCities();
+//   if (isLoading) return <Spinner />;
+//   console.log(cities);
+
+//   if (!cities.length)
+//     return (
+//       <Message message="Add your first city by clicking on a city on the map" />
+//     );
+
+//   const countries = cities.reduce((arr, city) => {
+//     if (!arr.map((el) => el.country).includes(city.country)) {
+//       return [...arr, { country: city.country, emoji: city.emoji }];
+//     } else return arr;
+//   }, []);
+
+//   return (
+//     <ul className={styles.countryList}>
+//       {countries.map((country) => (
+//         <CountryItem country={country} key={country.id} />
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export default CountryList;
+
 import Spinner from "./Spinner";
 import Message from "./Message";
-import styles from "./CountryList.module.css";
 import CountryItem from "./CountryItem";
-import { useCities } from "../contexts/CitiesContext";
 
-function CountryList() {
-  const { cities, isLoading } = useCities();
+import styles from "./CountryList.module.css";
+
+function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-  console.log(cities);
-
   if (!cities.length)
     return (
-      <Message message="Add your first city by clicking on a city on the map" />
+      <Message message="Add your first city by clicking on the a city on the map" />
     );
 
   const countries = cities.reduce((arr, city) => {
-    if (!arr.map((el) => el.country).includes(city.country)) {
-      return [...arr, { country: city.country, emoji: city.emoji }];
-    } else return arr;
+    if (arr.some((el) => el.country === city.country)) return arr;
+    return [...arr, { country: city.country, emoji: city.emoji }];
   }, []);
 
   return (
