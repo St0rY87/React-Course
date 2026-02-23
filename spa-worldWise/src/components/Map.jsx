a04a4db2462332c58f3b9821aca2a8bd3e4358d1
Dmitriy <mr.dima337@gmail.com>
2026-02-23 21:56:03 +0300

    feat spa-worldWise: displaying markers on map

--- spa-worldWise/src/components/Map.jsx
+++ spa-worldWise/src/components/Map.jsx
@@ -92,11 +92,13 @@ import {
   useMapEvents,
 } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
-  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams);
   const lat = searchParams.get("lat");
@@ -113,11 +115,14 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
