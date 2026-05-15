2132ff61c727ea51d797c5a6942b46a127007d50
Dmitriy <mr.dima337@gmail.com>
2026-05-15 16:11:21 +0300

    feat: created AppLayout

---
+++ fast-react-pizza/src/ui/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Header</p>
+    </header>
+  )
+}
+
+export default Header
