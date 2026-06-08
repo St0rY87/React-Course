0172cda6003f7a5afd2ce5ca1d2c35e83f127d67
Dmitriy <mr.dima337@gmail.com>
2026-06-08 22:38:06 +0300

    feat fast-react-pizza: added tailwind classes

--- fast-react-pizza/src/features/user/CreateUser.tsx
+++ fast-react-pizza/src/features/user/CreateUser.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-  function handleSubmit(e) {
+  function handleSubmit(e: any) {
     e.preventDefault();
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
-      {username !== '' && (
+      {username !== "" && (
         <div>
           <button>Start ordering</button>
         </div>
